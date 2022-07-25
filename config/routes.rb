d29c21e293d4a9594abf40d4c8f53a5d72b8e157
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-07-25 06:08:41 +0000

    devise複数管理機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,30 @@
 Rails.application.routes.draw do
+  
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers, skip:[:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  
+
+  
   root to: 'public/homes#top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
